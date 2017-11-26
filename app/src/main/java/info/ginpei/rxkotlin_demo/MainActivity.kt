163846c1f38fc274da6db8c9703a69ef98a6d28c
ginpei <ginpei@gmail.com>
2017-11-25 20:53:05 -0800

    RxJava!

--- app/src/main/java/info/ginpei/rxkotlin_demo/MainActivity.kt
+++ app/src/main/java/info/ginpei/rxkotlin_demo/MainActivity.kt
@@ -2,7 +2,20 @@ package info.ginpei.rxkotlin_demo
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import io.reactivex.Observable
+import io.reactivex.Single
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.concurrent.TimeUnit
+
+
+class DoSomething {
+    fun doSomething(s: String) {
+        println(s)
+    }
+}
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,8 +23,77 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        runCool.setOnClickListener { view ->
-            println("Hello!")
+        runJustHello.setOnClickListener {
+            val observer = Observable.just("Hello")
+            observer.subscribe { s ->
+                Toast.makeText(applicationContext, s, Toast.LENGTH_LONG).show()
+            }
+
+            println("Hello?")
+        }
+
+        sleepThread.setOnClickListener {
+            Toast.makeText(applicationContext, "Sleeping...", Toast.LENGTH_SHORT).show()
+
+            val thread = object : Thread() {
+                override fun run() {
+                    try {
+                        Thread.sleep(1000)
+                    } catch (e: InterruptedException) {
+                        e.printStackTrace()
+                    }
+
+                    runOnUiThread {
+                        Toast.makeText(applicationContext, "Woke up!", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+
+            thread.start()
+        }
+
+        sleepRx.setOnClickListener {
+            Toast.makeText(applicationContext, "Sleeping...", Toast.LENGTH_SHORT).show()
+
+            Single.create<String> { subscriber ->
+                println("Single.create()")
+                Thread.sleep(3000)
+                subscriber.onSuccess("Woke up!")
+            }
+                    .subscribeOn(Schedulers.io())
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribe({ message ->
+                        println("onSuccess")
+                        Toast.makeText(applicationContext, message, Toast.LENGTH_SHORT).show()
+                    }, {
+                        println("onError")
+                    })
+        }
+
+        countUntilWakingUp.setOnClickListener {
+            val countToast = Toast.makeText(applicationContext, "", Toast.LENGTH_LONG)
+            countToast.show()
+
+            Observable.create<String> { subscriber ->
+                println("Observable.create()")
+                for (count in 3 downTo 1) {
+                    subscriber.onNext(count.toString())
+                    Thread.sleep(1000)
+                }
+                subscriber.onComplete()
+            }
+                    .subscribeOn(Schedulers.io())
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribe({ sCount ->
+                        println("onNext ${sCount}")
+                        countToast.setText(sCount)
+                    }, {
+                        println("onError")
+                    }, {
+                        println("onComplete")
+                        countToast.cancel()
+                        Toast.makeText(applicationContext, "Woke up!", Toast.LENGTH_SHORT).show()
+                    })
         }
     }
 }
