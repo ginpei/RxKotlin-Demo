8ab9dcf2ee8021daa1a7b0011afd37e2be6c434a
ginpei <ginpei@gmail.com>
2017-11-25 22:55:48 -0800

    handle an array

--- app/src/main/java/info/ginpei/rxkotlin_demo/MainActivity.kt
+++ app/src/main/java/info/ginpei/rxkotlin_demo/MainActivity.kt
@@ -8,6 +8,7 @@ import io.reactivex.Single
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 import java.util.concurrent.TimeUnit
 
 
@@ -94,6 +95,32 @@ class MainActivity : AppCompatActivity() {
                         countToast.cancel()
                         Toast.makeText(applicationContext, "Woke up!", Toast.LENGTH_SHORT).show()
                     })
+        }
+
+        handleUserList.setOnClickListener {
+            class User(val name: String, val birthYear: Int, val active: Boolean)
+
+            val users = arrayListOf(
+                    User("Alice", 2000, true),
+                    User("Bob", 2011, true),
+                    User("Charlie", 1966, false),
+                    User("Derik", 1980, true),
+                    User("Emma", 1981, true),
+                    User("Fred", 1982, true),
+                    User("Ginpei", 1983, false),
+                    User("Hiroki", 1987, true)
+            )
+
+            Observable.fromIterable(users)
+                    .skipLast(1)  // remove Hiroki
+                    .filter { user -> user.active }  // remove Charlie and Ginpei
+                    .sorted { u1, u2 -> u2.birthYear - u1.birthYear }
+                    .skipLast(1)  // remove Derik
+                    .map { user -> "${user.name} (${2017 - user.birthYear})" }
+                    .subscribe({ text ->
+                        println("- ${text}")
+                    })
+            Toast.makeText(applicationContext, "Find result in logs", Toast.LENGTH_SHORT).show()
         }
     }
 }
